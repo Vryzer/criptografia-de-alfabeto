4cf27ec697e6d152cb7538cbfe495e8f46fd51e8
Vinicius Alves <aovini@hotmail.com>
2025-02-04 17:06:40 -0300

    Implementacao virgula, ponto, cedilha e quebra de linhas.

--- main.py
+++ main.py
@@ -29,7 +29,10 @@ mapa_criptografia = {
     "x": "letras/Xcriptografado.png",
     "y": "letras/Ycriptografado.png",
     "z": "letras/Zcriptografado.png",
-    " ": "letras/ESPACOcriptografado.png"
+    " ": "letras/ESPACOcriptografado.png",
+    "ç": "letras/Çcriptografado.png",
+    ",": "letras/VIRGULAcriptografado.png",
+    ".": "letras/PONTOFINALcriptografado.png"
 }
 
 def criar_png(texto):
@@ -47,19 +50,38 @@ def criar_png(texto):
         print("Nenhuma imagem válida encontrada.")
         return
 
-    # Calcula dimensões da imagem final concatenada
-    largura_total = sum(imagem.width for imagem in imagens)
-    altura_maxima = max(imagem.height for imagem in imagens)
+    #Determinando largura e altura em pixels, como um quarto de uma folha a4. Isso vai servir para contar as linhas caso fique uma mensagem enorme, pois sem isso ficaria tudo na mesma linha.
+    #Por curiosidade, um exemplo da folha a4, com 210mm x 297mm, convertendo para pixels, fica 2480 e 3508.
+    largura_maxima = 620
+    altura_maxima = 877
     
-    # Cria uma nova imagem com o tamanho combinado
-    nova_imagem = Image.new("RGBA", (largura_total, altura_maxima))
-    
-    # Adiciona cada imagem lado a lado
+    #inicializa as dimensões da imagem formada, tudo no ponto 0,0.
     x_offset = 0
-    for imagem in imagens:
-        nova_imagem.paste(imagem, (x_offset, 0))
-        x_offset += imagem.width
+    y_offset = 0
+    linha_atual_altura = 0
     
+ # A definição do RGBA está baseada na cor de fundo das fotos da pasta "letras". Caso você mude as letras e tenha uma outra cor de fundo, mude os valores usados em (217, 196, 142, 255)
+    nova_imagem = Image.new("RGBA", (largura_maxima, altura_maxima), (217, 196, 142, 255))
+
+    for imagem in imagens:
+        largura, altura = imagem.size
+
+        # Quebra de linha se a largura ultrapassar a máxima permitida
+        if x_offset + largura > largura_maxima:
+            x_offset = 0
+            y_offset += linha_atual_altura
+            linha_atual_altura = 0
+
+        # Verificar se a imagem ultrapassaria o limite da folha
+        if y_offset + altura > altura_maxima:
+            print("Conteúdo ultrapassou o limite da folha A4. Truncando...")
+            break
+
+        # Colar imagem na posição correta
+        nova_imagem.paste(imagem, (x_offset, y_offset))
+        x_offset += largura
+        linha_atual_altura = max(linha_atual_altura, altura)
+
     # Salva a imagem final
     nova_imagem.save("texto_criptografado.png")
     print("Imagem gerada com sucesso! Nome: texto_criptografado.png")
